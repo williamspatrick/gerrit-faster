233273ad581e68ad32841628f4f8c39e5de76b98
Patrick Williams <patrick@stwcx.xyz>
2025-02-28 15:26:06 -0500

    hello-world with axum
    Signed-off-by: Patrick Williams <patrick@stwcx.xyz>

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,22 @@
+use axum::{response::Html, routing::get, Router};
 use dotenv::dotenv;
 
-fn main() {
+#[tokio::main(flavor = "current_thread")]
+async fn main() {
     dotenv().ok();
 
-    println!("Hello, {}!", std::env::var("GERRIT_USERNAME").expect("USERNAME must be set"));
+    // build our application with a route
+    let app = Router::new().route("/", get(handler));
+
+    // run it
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
+        .await
+        .unwrap();
+    println!("listening on {}", listener.local_addr().unwrap());
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn handler() -> Html<String> {
+    let username = std::env::var("GERRIT_USERNAME").expect("USERNAME must be set");
+    Html(std::format!("Hello, {}!", username))
 }
