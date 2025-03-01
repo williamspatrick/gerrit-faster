95df4f8d0dfa52b6dce7cf0dcb733ffe1df11571
Patrick Williams <patrick@stwcx.xyz>
2025-03-01 17:10:09 -0500

    discord: add simple bot
    Signed-off-by: Patrick Williams <patrick@stwcx.xyz>

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,6 @@
+pub mod discord {
+    pub mod serve;
+}
 pub mod webserver {
     pub mod serve;
 }
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
 use dotenv::dotenv;
+use gerrit_faster::discord::serve as discord;
 use gerrit_faster::webserver::serve as webserver;
 use tokio;
+use tracing_subscriber;
 
 #[tokio::main(flavor = "current_thread")]
 async fn main() {
+    tracing_subscriber::fmt::init();
+
     dotenv().ok();
 
-    tokio::join!(webserver::serve());
+    tokio::join!(webserver::serve(), discord::serve());
 }
