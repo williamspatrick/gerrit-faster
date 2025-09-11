7e5fd902f0186184b58184f7849350ac74723bdf
Patrick Williams <patrick@stwcx.xyz>
2025-09-11 12:22:00 -0400

    changes: add more status checks
    Signed-off-by: Patrick Williams <patrick@stwcx.xyz>

--- src/changes/status.rs
+++ src/changes/status.rs
@@ -5,6 +5,9 @@ pub enum ReviewState {
     Unknown,
     PendingCI,
     FailingCI,
+    PendingFeedback,
+    PendingCommentResolution,
+    CommunityReview,
 }
 
 fn pending_ci(change: &GerritChange) -> bool {
@@ -14,7 +17,7 @@ fn pending_ci(change: &GerritChange) -> bool {
         }
         return score.value == 0;
     }
-    false
+    true
 }
 
 fn failing_ci(change: &GerritChange) -> bool {
@@ -27,6 +30,26 @@ fn failing_ci(change: &GerritChange) -> bool {
     false
 }
 
+fn pending_feedback(change: &GerritChange) -> bool {
+    for score in change.labels["Code-Review"].iter() {
+        return score.value < 0;
+    }
+    false
+}
+
+fn pending_comments(change: &GerritChange) -> bool {
+    change.unresolved_comment_count != 0
+}
+
+fn no_reviews(change: &GerritChange) -> bool {
+    for score in change.labels["Code-Review"].iter() {
+        if score.value != 0 {
+            return false;
+        }
+    }
+    true
+}
+
 pub fn review_state(change: &GerritChange) -> ReviewState {
     if pending_ci(change) {
         return ReviewState::PendingCI;
@@ -34,6 +57,18 @@ pub fn review_state(change: &GerritChange) -> ReviewState {
 
     if failing_ci(change) {
         return ReviewState::FailingCI;
+    }
+
+    if pending_feedback(change) {
+        return ReviewState::PendingFeedback;
+    }
+
+    if pending_comments(change) {
+        return ReviewState::PendingCommentResolution;
+    }
+
+    if no_reviews(change) {
+        return ReviewState::CommunityReview;
     }
 
     ReviewState::Unknown
