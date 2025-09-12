b60d7c152072541579827e26074e49b1994d8b28
Patrick Williams <patrick@stwcx.xyz>
2025-09-12 11:22:45 -0400

    webserve: add review-status route
    Mimic the review-status command from Discord into a webserver route.
    
    Signed-off-by: Patrick Williams <patrick@stwcx.xyz>

--- src/webserver/serve.rs
+++ src/webserver/serve.rs
@@ -1,12 +1,19 @@
+use crate::changes as Changes;
 use crate::context::ServiceContext;
 use crate::gerrit::connection::GerritConnection;
-use axum::{Extension, Router, response::Html, routing::get};
+use axum::{
+    Extension, Router,
+    extract::Path,
+    response::{Html, IntoResponse},
+    routing::get,
+};
 use tower::ServiceBuilder;
 
 pub async fn serve(context: ServiceContext) {
     // build our application with a route
     let app = Router::new()
         .route("/", get(root))
+        .route("/review-status/:id", get(review_status))
         .layer(ServiceBuilder::new().layer(Extension(context)));
 
     // run it
@@ -22,4 +29,33 @@ async fn root(Extension(context): Extension<ServiceContext>) -> Html<String> {
         "Connecting to Gerrit as '{}'!",
         context.get_gerrit().get_username()
     ))
+}
+
+async fn review_status(
+    Path(change_id): Path<String>,
+    Extension(context): Extension<ServiceContext>,
+) -> impl IntoResponse {
+    let change: Option<Changes::container::Change>;
+    {
+        let changes = &context.lock().unwrap().changes;
+
+        let id = change_id.parse::<u64>();
+        change = match id {
+            Ok(i) => changes.get(i),
+            _ => changes.get_by_change_id(&change_id),
+        }
+    }
+
+    if let Some(change) = change {
+        Html(std::format!(
+            "<html><body><h1>Change {}</h1><p>Review Status: {:?}</p></body></html>",
+            change_id,
+            change.review_state
+        )).into_response()
+    } else {
+        (axum::http::StatusCode::NOT_FOUND, Html(std::format!(
+            "<html><body><h1>Change Not Found</h1><p>Could not find change: {}</p></body></html>",
+            change_id
+        ))).into_response()
+    }
 }
