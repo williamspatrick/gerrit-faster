8e72d354d5c433be11a94c0c74bcedd5c70b6565
Patrick Williams <patrick@stwcx.xyz>
2025-09-19 17:15:23 -0400

    webserve: add a port argument
    Allow a `--port <port>` to run the service on a specific TCP port;
    continue to default to 3000.
    
    Signed-off-by: Patrick Williams <patrick@stwcx.xyz>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use clap::Parser;
 use dotenv::dotenv;
 use gerrit_faster::changes::serve as changes;
 use gerrit_faster::context::ServiceContext;
@@ -7,8 +8,18 @@ use tokio;
 use tracing::info;
 use tracing_subscriber;
 
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+    /// Port to run the webserver on
+    #[clap(short, long, default_value_t = 3000)]
+    port: u16,
+}
+
 #[tokio::main(flavor = "current_thread")]
 async fn main() {
+    let args = Args::parse();
+
     tracing_subscriber::fmt::init();
 
     dotenv().ok();
@@ -17,7 +28,7 @@ async fn main() {
     info!("Service ServiceContext: {:?}", context);
 
     tokio::join!(
-        webserver::serve(context.clone()),
+        webserver::serve(context.clone(), args.port),
         discord::serve(context.clone()),
         changes::serve(context.clone())
     );
