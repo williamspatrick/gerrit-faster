c3616cb2ca6e1ca01e978eae10f54ef1b5ae34b5
Patrick Williams <patrick@stwcx.xyz>
2025-09-20 09:39:00 -0400

    gerrit: handle errors with retry
    When we get an error from Gerrit, go ahead and retry it every 10
    seconds.  This handles issues with Gerrit itself.  For errors in
    JSON parsing, continue to abort but give more output in the JSON
    failure.
    
    For the abandon operation specifically, Gerrit can return a status
    409 (Conflict) when the abandon is not successful.  Do not retry on
    this nor crash but just record it as happening.
    
    Signed-off-by: Patrick Williams <patrick@stwcx.xyz>

--- src/changes/serve.rs
+++ src/changes/serve.rs
@@ -23,21 +23,22 @@ async fn abandon_older_than_two_years(
     );
 
     // Call the abandon_change method
-    match context
-            .get_gerrit()
-            .abandon_change(
-                &change.id,
-                concat!(
-                    "Automatically abandoned due to inactivity of over two years.\n",
-                    "Please rebase and reopen if this should still be merged.",
-                ).to_string(),
-            )
-            .await
+    if let Some(_) = context
+        .get_gerrit()
+        .abandon_change(
+            &change.id,
+            concat!(
+                "Automatically abandoned due to inactivity of over two years.\n",
+                "Please rebase and reopen if this should still be merged.",
+            ).to_string(),
+        )
+        .await
     {
-        Ok(_) => { info!("Successfully abandoned change {}", change.id); return true; },
-        Err(e) => info!("Failed to abandon change {}: {}", change.id, e),
+        info!("Successfully abandoned change {}", change.id);
+        return true;
     }
 
+    info!("Was not able to abandon change {}.", change.id);
     false
 }
 
@@ -72,28 +73,28 @@ async fn abandon_older_than_one_year_and_bad_ci(
     );
 
     // Call the abandon_change method
-    match context
-                .get_gerrit()
-                .abandon_change(
-                    &change.id,
-                    concat!(
-                        "Automatically abandoned due to missing or failing CI and inactivity of over one year.\n",
-                        "Please rebase, resolve CI and reopen if this should still be merged.\n",
-                        "\n",
-                    )
-                    .to_string()
-                    + &format!(
-                        "CI status: {}={}",
-                        ci_failure.username,
-                        ci_failure.value
-                    )
-                )
-                .await
-            {
-                Ok(_) => { info!("Successfully abandoned change {}", change.id); return true},
-                Err(e) => info!("Failed to abandon change {}: {}", change.id, e),
-            }
-
+    if let Some(_) = context
+        .get_gerrit()
+        .abandon_change(
+            &change.id,
+            concat!(
+                "Automatically abandoned due to missing or failing CI and inactivity of over one year.\n",
+                "Please rebase, resolve CI and reopen if this should still be merged.\n",
+                "\n",
+            )
+            .to_string()
+            + &format!(
+                "CI status: {}={}",
+                ci_failure.username,
+                ci_failure.value
+            )
+        )
+        .await
+    {
+        info!("Successfully abandoned change {}", change.id);
+        return true;
+    }
+    info!("Was not able to abandon change {}", change.id);
     false
 }
 
@@ -103,9 +104,9 @@ pub async fn serve(context: ServiceContext) {
     loop {
         let changes = if first_time {
             first_time = false;
-            context.get_gerrit().all_open_changes().await.unwrap()
+            context.get_gerrit().all_open_changes().await
         } else {
-            context.get_gerrit().recent_changes().await.unwrap()
+            context.get_gerrit().recent_changes().await
         };
 
         for change in &changes {
