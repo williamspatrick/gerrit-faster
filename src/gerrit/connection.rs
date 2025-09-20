ba5a0eae6087891ee0951588c475e55f4799855c
Patrick Williams <patrick@stwcx.xyz>
2025-09-20 09:39:00 -0400

    gerrit: handle errors with retry
    When we get an error from Gerrit, go ahead and retry it every 10
    seconds.  This handles issues with Gerrit itself.  For errors in
    JSON parsing, continue to abort but give more output in the JSON
    failure.
    
    For the abandon operation specifically, Gerrit can return a status
    409 (Conflict) when the abandon is not successful.  Do not retry on
    this nor crash but just record it as happening.
    
    Signed-off-by: Patrick Williams <patrick@stwcx.xyz>

--- src/gerrit/connection.rs
+++ src/gerrit/connection.rs
@@ -1,36 +1,37 @@
 use crate::gerrit::data as gerrit_data;
 use serde_json;
 use std::fmt;
+use tokio::time::{self, Duration};
+use tracing::{error, info};
 
 /* Gerrit JSON responses have a magic at the beginning that needs to be
  * stripped. */
-fn prune_magic(text: String) -> String {
+fn prune_magic(text: String) -> Option<String> {
     const MAGIC_PREFIX: &str = ")]}'";
     if text.starts_with(MAGIC_PREFIX) {
-        return text[MAGIC_PREFIX.len()..].to_string();
+        return Some(text[MAGIC_PREFIX.len()..].to_string());
     }
-    text
+    None
 }
 
 #[async_trait::async_trait]
 pub trait GerritConnection {
     fn get_username(&self) -> String;
     fn get_password(&self) -> String;
-    async fn execute_request(
+    async fn execute_request<F>(
         &self,
         request: reqwest::RequestBuilder,
-    ) -> Result<String, reqwest::Error>;
-    async fn all_open_changes(
-        &self,
-    ) -> Result<Vec<gerrit_data::ChangeInfo>, reqwest::Error>;
-    async fn recent_changes(
-        &self,
-    ) -> Result<Vec<gerrit_data::ChangeInfo>, reqwest::Error>;
+        acceptable_status: F,
+    ) -> String
+    where
+        F: Fn(reqwest::StatusCode) -> bool + Send;
+    async fn all_open_changes(&self) -> Vec<gerrit_data::ChangeInfo>;
+    async fn recent_changes(&self) -> Vec<gerrit_data::ChangeInfo>;
     async fn abandon_change(
         &self,
         change_id: &str,
         message: String,
-    ) -> Result<gerrit_data::ChangeInfo, reqwest::Error>;
+    ) -> Option<gerrit_data::ChangeInfo>;
 }
 
 pub struct Connection {
@@ -66,55 +67,104 @@ impl GerritConnection for Connection {
         self.password.clone()
     }
 
-    async fn execute_request(
+    async fn execute_request<F>(
         &self,
         request: reqwest::RequestBuilder,
-    ) -> Result<String, reqwest::Error> {
-        Ok(prune_magic(
+        acceptable_status: F,
+    ) -> String
+    where
+        F: Fn(reqwest::StatusCode) -> bool + Send,
+    {
+        // Clone the request builder for retries
+        let request_factory = || {
             request
+                .try_clone()
+                .expect("Failed to clone request builder")
+        };
+
+        loop {
+            let current_request = request_factory();
+            match current_request
                 .basic_auth(self.get_username(), Some(self.get_password()))
                 .send()
-                .await?
-                .text()
-                .await?,
-        ))
+                .await
+            {
+                Ok(response) => {
+                    // Check if this is an exception status that should not be retried
+                    if acceptable_status(response.status()) {
+                        let status = response.status();
+                        let error_text = response
+                            .text()
+                            .await
+                            .unwrap_or_else(|_| "Unknown error".to_string());
+                        info!(
+                            "Request failed with exception status {}: {}",
+                            status, error_text
+                        );
+                        // For exception statuses, return empty string
+                        return String::new();
+                    }
+
+                    let text = response.text().await.unwrap();
+                    if let Some(pruned) = prune_magic(text) {
+                        return pruned;
+                    }
+                    // If we don't get the magic prefix, wait 10 seconds and retry
+                    time::sleep(Duration::from_secs(10)).await;
+                }
+                Err(_e) => {
+                    // If we get an HTTP error, wait 10 seconds and retry
+                    time::sleep(Duration::from_secs(10)).await;
+                }
+            }
+        }
     }
 
-    async fn all_open_changes(
-        &self,
-    ) -> Result<Vec<gerrit_data::ChangeInfo>, reqwest::Error> {
-        let result = self.execute_request(reqwest::Client::new()
-            .get("https://gerrit.openbmc.org/a/changes/?q=status:open+-is:wip&o=LABELS&o=DETAILED_ACCOUNTS&no-limit")).await?;
+    async fn all_open_changes(&self) -> Vec<gerrit_data::ChangeInfo> {
+        let result = self
+            .execute_request(
+                reqwest::Client::new().get(
+                    "https://gerrit.openbmc.org/a/changes/?q=status:open+-is:wip&o=LABELS&o=DETAILED_ACCOUNTS&no-limit",
+                ),
+                |_| false,
+            )
+            .await;
 
-        Ok(
-            serde_json::from_str::<Vec<gerrit_data::ChangeInfoRaw>>(&result)
-                .expect("JSON failed")
-                .into_iter()
-                .map(Into::into)
-                .collect(),
-        )
+        serde_json::from_str::<Vec<gerrit_data::ChangeInfoRaw>>(&result)
+            .expect(&format!(
+                "Failed to parse JSON response for all_open_changes. Response content: {}",
+                result
+            ))
+            .into_iter()
+            .map(Into::into)
+            .collect()
     }
 
-    async fn recent_changes(
-        &self,
-    ) -> Result<Vec<gerrit_data::ChangeInfo>, reqwest::Error> {
-        let result = self.execute_request(reqwest::Client::new()
-            .get("https://gerrit.openbmc.org/a/changes/?q=-age:1h&o=LABELS&o=DETAILED_ACCOUNTS&no-limit")).await?;
+    async fn recent_changes(&self) -> Vec<gerrit_data::ChangeInfo> {
+        let result = self
+            .execute_request(
+                reqwest::Client::new().get(
+                    "https://gerrit.openbmc.org/a/changes/?q=-age:4h&o=LABELS&o=DETAILED_ACCOUNTS&no-limit",
+                ),
+                |_| false,
+            )
+            .await;
 
-        Ok(
-            serde_json::from_str::<Vec<gerrit_data::ChangeInfoRaw>>(&result)
-                .expect("JSON failed")
-                .into_iter()
-                .map(Into::into)
-                .collect(),
-        )
+        serde_json::from_str::<Vec<gerrit_data::ChangeInfoRaw>>(&result)
+            .expect(&format!(
+                "Failed to parse JSON response for recent_changes. Response content: {}",
+                result
+            ))
+            .into_iter()
+            .map(Into::into)
+            .collect()
     }
 
     async fn abandon_change(
         &self,
         change_id: &str,
         message: String,
-    ) -> Result<gerrit_data::ChangeInfo, reqwest::Error> {
+    ) -> Option<gerrit_data::ChangeInfo> {
         let url = format!(
             "https://gerrit.openbmc.org/a/changes/{}/abandon",
             change_id
@@ -124,18 +174,31 @@ impl GerritConnection for Connection {
         request_body
             .insert("message".to_string(), serde_json::Value::String(message));
 
+        // Use execute_request with exception handling for HTTP 409 errors
         let result = self
             .execute_request(
                 reqwest::Client::new()
                     .post(&url)
                     .header("Content-Type", "application/json")
                     .json(&request_body),
+                |status| status == reqwest::StatusCode::CONFLICT,
             )
-            .await?;
+            .await;
 
-        Ok(serde_json::from_str::<gerrit_data::ChangeInfoRaw>(&result)
-            .expect("JSON failed")
-            .into())
+        // If we got an empty string, it means we hit an exception status (409)
+        if result.is_empty() {
+            error!("Failed to abandon change {} due to conflict", change_id);
+            return None;
+        }
+
+        Some(
+            serde_json::from_str::<gerrit_data::ChangeInfoRaw>(&result)
+                .expect(&format!(
+                    "Failed to parse JSON response for abandon_change. Response content: {}",
+                    result
+                ))
+                .into(),
+        )
     }
 }
 
