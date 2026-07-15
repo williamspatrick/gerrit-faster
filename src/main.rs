6a9fb73136211136ccebb31a63deb783f88e5279
Patrick Williams <patrick@stwcx.xyz>
2026-07-15 18:10:51 -0400

    main: add option to disable discord
    Signed-off-by: Patrick Williams <patrick@stwcx.xyz>

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,7 @@ use gerrit_faster::changes::serve as changes;
 use gerrit_faster::context::ServiceContext;
 use gerrit_faster::discord::serve as discord;
 use gerrit_faster::webserver::serve as webserver;
-use tokio;
 use tracing::{Level, info};
-use tracing_subscriber;
 
 #[derive(Parser, Debug)]
 #[clap(author, version, about, long_about = None)]
@@ -17,6 +15,9 @@ struct Args {
     /// Debug mode
     #[clap(short, long, default_value_t = false)]
     debug: bool,
+    /// Disable the Discord bot
+    #[clap(long, default_value_t = false)]
+    disable_discord: bool,
 }
 
 #[tokio::main(flavor = "current_thread")]
@@ -36,9 +37,16 @@ async fn main() {
     let context = ServiceContext::new();
     info!("Service ServiceContext: {:?}", context);
 
-    tokio::join!(
-        webserver::serve(context.clone(), args.port),
-        discord::serve(context.clone()),
-        changes::serve(context.clone())
-    );
+    let mut handles = vec![
+        tokio::spawn(webserver::serve(context.clone(), args.port)),
+        tokio::spawn(changes::serve(context.clone())),
+    ];
+
+    if !args.disable_discord {
+        handles.push(tokio::spawn(discord::serve(context.clone())));
+    }
+
+    for handle in handles {
+        handle.await.unwrap();
+    }
 }
